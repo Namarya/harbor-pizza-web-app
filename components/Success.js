792ab8942a9189deea03dfdc79e0f834b3ad9cde
Namarya <126215060+Namarya@users.noreply.github.com>
2023-05-29 16:16:24 -0700

    Delete components directory

--- components/Success.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Success({ success }) {
-  return (
-    <div className="alert alert-success" role="alert">
-      {success}
-    </div>
-  );
-}
